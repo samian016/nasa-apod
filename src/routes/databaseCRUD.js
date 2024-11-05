7a42a590c80ad4015922ba90d6f2531d232d7766
samian016 <walidbinjashim@gmail.com>
2024-11-05 12:11:44 +0600

    vercel json schedular add

--- src/routes/databaseCRUD.js
+++ src/routes/databaseCRUD.js
@@ -1,9 +1,11 @@
 const asyncMiddleware = require("../middlewares/asyncMiddleware");
-const { getAllPost, updateToken } = require("../service.js/apodPost");
+const { getAllPost, updateToken, schedulerPost, schedulerUpdateToken } = require("../service.js/apodPost");
 
 function databaseCRUD(app) {
   app.get("/api/v1/apod-posts", asyncMiddleware(getAllPost));
   app.post("/api/v1/update-token", asyncMiddleware(updateToken));
+  app.get("/api/v1/scheduler-update-token", asyncMiddleware(schedulerUpdateToken));
+  app.get("/api/v1/scheduler-post", asyncMiddleware(schedulerPost));
 };
 
 exports.databaseCRUD = databaseCRUD;
--- src/service.js/apodPost.js
+++ src/service.js/apodPost.js
@@ -39,7 +39,69 @@ const updateToken = async (req, res) => {
   return res.status(200).send(resp);
 }
 
+
+const schedulerUpdateToken = async (req, res) => {
+  const config = await Token.findOne();
+  const { access_token, client_id, client_secret } = config._doc;
+  // console.log("access_token")
+  if (!config) {
+    return console.log("[-] No token found");
+  } else {
+    let resp;
+    try {
+      resp = await request('GET', `https://graph.facebook.com/v17.0/oauth/access_token?grant_type=fb_exchange_token&client_id=${client_id}&client_secret=${client_secret}&fb_exchange_token=${access_token}`);
+    } catch (err) {
+      return console.log(err);
+    }
+    await Token.findByIdAndUpdate(config._id.toString(), { token: resp.token, access_token: resp.access_token, expires_in: resp.expires_in, token_type: resp.token_type }, { new: true });
+    console.log('Token updated');
+    return res.status(200).send('Token updated');
+  }
+}
+
+const schedulerPost = async (req, res) => {
+  const config = await Token.findOne();
+  if (!config) {
+    console.error("[-] No token found for initial post!");
+  }
+  const { nasaToken, access_token } = config._doc;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${nasaToken}`;
+  const dateToday = getUSADate();
+  const existInitialApod = await ApodPost.findOne({ date: dateToday });
+  if (existInitialApod) {
+    console.log("[-] APOD already exist");
+    res.status(200).send("[-] APOD already exist");
+  } else {
+    try {
+      const initialResp = await request('GET', url);
+      const { hdurl } = initialResp;
+      const messageStr = formatPostCaption(initialResp);
+
+      console.log("[+] title created");
+      const fbUrl = `https://graph.facebook.com/v21.0/482510824938423/photos?access_token=${access_token}`;
+      const initialPost = await request('POST', fbUrl, {
+        message: messageStr,
+        url: hdurl
+      });
+      if (initialPost.post_id) {
+        console.log("[+] Post On Facebook success");
+        const schema = ApodPost(initialResp)
+        if (!schema) {
+          console.log("[-]Post not saved");
+        }
+        await schema.save()
+      }
+      return res.status(200).send("[+] Post On Facebook success");
+    } catch (er) {
+      console.log(er.message);
+      return res.status(400).send(er.message);
+    }
+  }
+}
+
 module.exports = {
   getAllPost,
-  updateToken
+  updateToken,
+  schedulerUpdateToken,
+  schedulerPost
 }
